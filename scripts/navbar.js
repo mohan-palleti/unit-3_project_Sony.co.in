a8fb87ec7827b32bec12248b117d4482f4b651b3
chandrakant Deore <87793194+chandudeore@users.noreply.github.com>
2022-01-21 09:49:50 +0530

    Add files via upload

---
+++ scripts/navbar.js
@@ -0,0 +1,33 @@
+function navbar(){
+    return ` <nav class="navbar ">
+    <ul class="nav_list v_class_nav">
+      <a href=""
+        ><img
+          src="https://1000logos.net/wp-content/uploads/2017/06/Symbol-Sony.jpg"
+          alt="logo"
+          id="logo_sony"
+          style="max-width: 100%; padding: 0px"
+      /></a>
+      <li id="dropdown_button">
+        <a href="">Electronics</a>
+       
+        </div>
+      </li>
+      <li><a href="">Play Station </a></li>
+      <li><a href="">Support</a></li>
+    </ul>
+    <ul class="right_nav v_class_nav">
+      <li class="my_sony_drop">
+        <p>My Sony</p>
+       
+      </li>
+      <li><i class="fas fa-heart"></i></li>
+      <li><p id="search_sony">Search Sony</p></li>
+      <li>
+        <p class="sony_sites">Sony Sites <i class="fa fa-caret-down"></i></p>
+      </li>
+    </ul>
+  </nav>`
+}
+
+export {navbar}
