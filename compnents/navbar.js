10e152be6fb0029b673527ab0ceb09a69b76e769
Shubham Jatar <52294167+shubham-jatar@users.noreply.github.com>
2022-01-21 15:12:41 +0530

    support page work

---
+++ compnents/navbar.js
@@ -0,0 +1,131 @@
+function navbar()
+{
+
+    return ` <nav class="nav_sm">
+    <a href=""
+      ><img
+        src="https://1000logos.net/wp-content/uploads/2017/06/Symbol-Sony.jpg"
+        alt="logo"
+        id="logo_sony"
+        style="max-width: 100%; padding: 0px"
+    /></a>
+
+    <div class="burger">
+      <div class="line"></div>
+      <div class="line"></div>
+      <div class="line"></div>
+    </div>
+  </nav>
+
+  <nav class="navbar navbar_sm">
+    <ul class="nav_list v_class_nav">
+      <a href=""
+        ><img
+          src="https://1000logos.net/wp-content/uploads/2017/06/Symbol-Sony.jpg"
+          alt="logo"
+          id="logo_sony"
+          style="max-width: 100%; padding: 0px"
+      /></a>
+      <li id="dropdown_button">
+        <a href="">Electronics</a>
+        <div class="dropdown_list">
+          <div class="container_list">
+            <div class="list_box1">
+              <div>
+                <ul>
+                  <span>Televisions & Home Cinema </span>
+                  <li>Televisions</li>
+                  <li>Home Theatre & Soundbars</li>
+                  <li>Blu-ray Disc <sup>TM</sup> DVD Players</li>
+                  <br />
+                  <a href="#">View All</a>
+                </ul>
+              </div>
+
+              <div>
+                <ul>
+                  <span>Audio </span>
+                  <li>HeadPhones</li>
+                  <li>MP3 Players</li>
+                  <li>High Resolution audio</li>
+                  <li>Wireless speakers</li>
+                  <a href="#">View All</a>
+                </ul>
+              </div>
+
+              <div>
+                <ul>
+                  <span>Cameras </span>
+                  <li>Interchangeable Lens Cameras</li>
+                  <li>Lenses</li>
+                  <li>Compact Cameras</li>
+                  <li>Cinema Line Cameras</li>
+                  <a href="#">View All</a>
+                </ul>
+              </div>
+
+              <div>
+                <ul>
+                  <span>Video Cameras </span>
+                  <li>Camcoders</li>
+                  <li>Action Cameras</li>
+                  <li>Professional Video Cameras</li>
+                  <li>Cinema Line Cameras</li>
+                  <a href="#">View All</a>
+                </ul>
+              </div>
+            </div>
+            <div class="list_box1">
+              <div>
+                <ul>
+                  <span>Car Audio </span>
+                  <li>Mobile ES <sup>TM</sup> Series</li>
+                  <li>Receivers and Players</li>
+                  <li>Amplifiers</li>
+                  <li>Speakers And Subwoofers</li>
+                  <a href="#">View All</a>
+                </ul>
+              </div>
+
+              <div>
+                <ul>
+                  <span>Storage & Cables </span>
+                  <li>Memory Cards & SSD</li>
+                  <li>Cables</li>
+                  <br />
+                  <br />
+                  <a href="#">View All</a>
+                </ul>
+              </div>
+            </div>
+            <div class="all_product_btn">
+              <button><i class="fas fa-th"></i> All Electronics</button>
+              <button>See Professional and Solutions</button>
+            </div>
+            <div class="map_details"></div>
+          </div>
+        </div>
+      </li>
+      <li><a href="">Play Station </a></li>
+      <li><a href="">Support</a></li>
+    </ul>
+    <ul class="right_nav v_class_nav">
+      <li class="my_sony_drop">
+        <p>My Sony</p>
+        <div class="signup_dropdown">
+          <button>Sign in</button>
+          <p>NewsLetter</p>
+          <p>Community</p>
+        </div>
+      </li>
+      <li><i class="fas fa-heart"></i></li>
+      <li><p id="search_sony">Search Sony</p></li>
+      <li>
+        <p class="sony_sites">Sony Sites <i class="fa fa-caret-down"></i></p>
+      </li>
+    </ul>
+  </nav>
+
+`
+}
+export default navbar
